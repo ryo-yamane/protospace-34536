3a6b0406579ec89a5721eb2157e64f6c84cd538d
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-02-17 11:46:53 +0900

    修正：トップページ

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :edit, :show]
-  before_action :move_to_index, except: [:index]
-
+  before_action :authenticate_user!, only: [:new, :create, :edti]
+ 
 
  def index
   @prototypes = Prototype.includes(:user)
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   resources :users, only: :show
   root to: "prototypes#index"
 
-
   
 end
