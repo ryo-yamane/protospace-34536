733b3b32ec7a08f7e8d14ae74fe268085be1a70d
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-02-17 16:08:48 +0900

    修正：リダイレクト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to root_path
+      redirect_to "/prototypes/#{@comment.prototype.id}"
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
